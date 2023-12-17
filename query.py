3c91786e9070bb754af1469244420bce1ef9d7ce
Steve Castellotti <sc@jigsaw.studio>
2023-12-17 05:19:59 -0800

    configuration file support added

--- query.py
+++ query.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+# Copyright (c) 2023 Steve Castellotti
+# This file is part of Urcuchillay and is released under the MIT License.
+# See LICENSE file in the project root for full license information.
+
 import argparse
 import logging
 import os
 import sys
+
+import config
 
 try:
     import llama_index
@@ -13,83 +19,6 @@ except ModuleNotFoundError as e:
     print('pyenv activate urcuchillay-env\n')
     sys.exit(1)
 
-DEBUG = False
-
-ENABLE_GPU = 1
-TEMPERATURE = 0.1
-MAX_NEW_TOKENS = 256
-CONTEXT_WINDOW = 3900  # Llama 2 has a context window of 4096 tokens, set below for safety
-
-MODELS = {
-    'llama-2-7b-chat.Q4_K_M.gguf': {  # (Q4_K_M: 4-bit, medium, balanced quality - recommended)
-        'model': 'https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF',
-        'url': 'https://huggingface.co/TheBloke/Llama-2-7B-chat-GGUF/' +
-               'resolve/main/llama-2-7b-chat.Q4_K_M.gguf',
-        'prompt_template': '{prompt}',
-        'model_creator': 'Meta',
-        'model_provider': 'TheBloke',
-    },
-    'llama-2-13b-chat.Q4_K_M.gguf': {  # (Q4_K_M: 4-bit, medium, balanced quality - recommended)
-        'model': 'https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF',
-        'url': 'https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/' +
-               'resolve/main/llama-2-13b-chat.Q4_K_M.gguf',
-        'prompt_template': '{prompt}',
-        'model_creator': 'Meta',
-        'model_provider': 'TheBloke',
-    },
-    'llama-2-70b-chat.Q4_K_M.gguf': {  # (Q4_K_M: 4-bit, medium, balanced quality - recommended)
-        'model': 'https://huggingface.co/TheBloke/Llama-2-70B-Chat-GGUF',
-        'url': 'https://huggingface.co/TheBloke/Llama-2-70B-chat-GGUF/' +
-               'resolve/main/llama-2-70b-chat.Q4_K_M.gguf',
-        'prompt_template': '{prompt}',
-        'model_creator': 'Meta',
-        'model_provider': 'TheBloke',
-    },
-    'mistral-7b-v0.1.Q4_K_M.gguf': {  # (Q4_K_M: 4-bit, medium, balanced quality - recommended)
-        'model': 'https://huggingface.co/TheBloke/Mistral-7B-v0.1-GGUF',
-        'url': 'https://huggingface.co/TheBloke/Mistral-7B-v0.1-GGUF/resolve/main/mistral-7b-v0.1.Q4_K_M.gguf',
-        'prompt_template': '[INST] {prompt} [/INST]',
-        'model_creator': 'Mistral AI',
-        'model_provider': 'TheBloke',
-    },
-    'mixtral-8x7b-instruct-v0.1.Q4_K_M.gguf': {  # (Q4_K_M: 4-bit, medium, balanced quality - recommended)
-        'model': 'https://huggingface.co/TheBloke/Mixtral-8x7B-Instruct-v0.1-GGUF',
-        'url': 'https://huggingface.co/TheBloke/Mixtral-8x7B-Instruct-v0.1-GGUF/' +
-               'resolve/main/mixtral-8x7b-instruct-v0.1.Q4_K_M.gguf',
-        'prompt_template': '[INST] {prompt} [/INST]',
-        'model_creator': 'Mistral AI',
-        'model_provider': 'TheBloke',
-    },
-}
-
-MODEL_ALIASES = {
-    '7': 'llama-2-7b-chat.Q4_K_M.gguf',
-    '7b': 'llama-2-7b-chat.Q4_K_M.gguf',
-    'llama-2-7b': 'llama-2-7b-chat.Q4_K_M.gguf',
-    '13': 'llama-2-13b-chat.Q4_K_M.gguf',
-    '13b': 'llama-2-13b-chat.Q4_K_M.gguf',
-    'llama-2-13b': 'llama-2-13b-chat.Q4_K_M.gguf',
-    '70': 'llama-2-70b-chat.Q4_K_M.gguf',
-    '70b': 'llama-2-70b-chat.Q4_K_M.gguf',
-    'llama-2-70b': 'llama-2-70b-chat.Q4_K_M.gguf',
-    'small': 'llama-2-7b-chat.Q4_K_M.gguf',
-    'medium': 'llama-2-13b-chat.Q4_K_M.gguf',
-    'large': 'llama-2-70b-chat.Q4_K_M.gguf',
-    'mistral': 'mistral-7b-v0.1.Q4_K_M.gguf',
-    'mistral-7b': 'mistral-7b-v0.1.Q4_K_M.gguf',
-    'mixtral': 'mixtral-8x7b-instruct-v0.1.Q4_K_M.gguf',
-    'mixtral-instruct': 'mixtral-8x7b-instruct-v0.1.Q4_K_M.gguf',
-    'mixtral-8x7b': 'mixtral-8x7b-instruct-v0.1.Q4_K_M.gguf',
-    'mixtral-8x7b-instruct': 'mixtral-8x7b-instruct-v0.1.Q4_K_M.gguf',
-}
-
-MODEL_URL_DEFAULT = MODELS[MODEL_ALIASES['7']]['url']
-
-EMBED_MODEL_NAME = 'local'
-
-DATA_PATH = os.path.join(os.getcwd(), 'data')
-MODEL_PATH = os.path.join(os.getcwd(), 'models')
-STORAGE_PATH = os.path.join(os.getcwd(), 'storage')
 
 DEFAULT_PROMPT = 'What is Urcuchillay?'
 
@@ -194,8 +123,8 @@ class Query:
     def display_exchange(self, query):
         print('Query: %s\n' % query)
 
-        if self.model_name in MODELS.keys():
-            query = MODELS[self.model_name]['prompt_template'].replace('{prompt}', query)
+        if self.model_name in config.Models.MODELS.keys():
+            query = config.Models.MODELS[self.model_name]['prompt_template'].replace('{prompt}', query)
             if self.debug:
                 print('Query (prompt): %s\n' % query)
 
@@ -203,12 +132,12 @@ class Query:
         print('Response: %s\n' % str(response).strip())
 
 
-def str2bool(v):
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ('yes', 'true', 'on', 't', 'y', '1'):
+def str2bool(arg):
+    if isinstance(arg, bool):
+        return arg
+    if arg.lower() in ('yes', 'true', 'on', 't', 'y', '1'):
         return True
-    elif v.lower() in ('no', 'false', 'off', 'f', 'n', '0'):
+    elif arg.lower() in ('no', 'false', 'off', 'f', 'n', '0'):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
@@ -216,33 +145,33 @@ def str2bool(v):
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Process command parameters')
-    parser.add_argument('--debug', type=str2bool, nargs='?', const=True, default=DEBUG,
+    parser.add_argument('--debug', type=str2bool, nargs='?', const=True, default=config.Config.DEBUG,
                         help='Enable debug mode (default: %(default)s)')
     parser.add_argument('-p', '--prompt', type=str, default=DEFAULT_PROMPT,
                         help='The prompt to process (default: %(default)s)')
-    parser.add_argument('--cpu', action='store_const', const=0, default=ENABLE_GPU,
+    parser.add_argument('--cpu', action='store_const', const=0, default=config.Config.ENABLE_GPU,
                         help='Use the CPU only instead of GPU acceleration')
-    parser.add_argument('--temperature', type=float, default=TEMPERATURE,
+    parser.add_argument('--temperature', type=float, default=config.Config.TEMPERATURE,
                         help='The temperature value for the model (default: %(default)s)')
-    parser.add_argument('--max_new_tokens', type=float, default=MAX_NEW_TOKENS,
+    parser.add_argument('--max_new_tokens', type=float, default=config.Config.MAX_NEW_TOKENS,
                         help='The max new tokens value for the model (default: %(default)s)')
-    parser.add_argument('--context', '--context_window', type=float, default=CONTEXT_WINDOW,
+    parser.add_argument('--context', '--context_window', type=float, default=config.Config.CONTEXT_WINDOW,
                         help='The context window value for the model (default: %(default)s)')
     parser.add_argument('--save', type=str2bool, nargs='?', const=True, default=False,
                         help='Save indexed vector store locally (default: %(default)s)')
     parser.add_argument('--load', type=str2bool, nargs='?', const=True, default=False,
                         help='Load indexed vector store (default: %(default)s)')
-    parser.add_argument('--data', '--data_path', type=str, default=DATA_PATH,
+    parser.add_argument('--data', '--data_path', type=str, default=config.Config.DATA_PATH,
                         help='The path to data files to be indexed (default: %(default)s)')
-    parser.add_argument('--path', '--model_path', type=str, default=MODEL_PATH,
+    parser.add_argument('--path', '--model_path', type=str, default=config.Config.MODEL_PATH,
                         help='The path to the directory for cached models (default: %(default)s)')
-    parser.add_argument('--storage', '--storage_path', type=str, default=STORAGE_PATH,
+    parser.add_argument('--storage', '--storage_path', type=str, default=config.Config.STORAGE_PATH,
                         help='The path to save and load the vector store (default: %(default)s)')
-    parser.add_argument('--model_url', type=str, default=MODEL_URL_DEFAULT,
+    parser.add_argument('--model_url', type=str, default=config.Config.MODEL_URL_DEFAULT,
                         help='Custom URL for model (defaults to the Llama-2-7b model)')
-    parser.add_argument('--model', '--model_name', type=str, default=None,
+    parser.add_argument('--model', '--model_name', type=str, default=config.Config.MODEL_DEFAULT,
                         help='The name of the model to use (default: extracted from model url)')
-    parser.add_argument('--embed_model_name', type=str, default=EMBED_MODEL_NAME,
+    parser.add_argument('--embed_model_name', type=str, default=config.Config.EMBED_MODEL_NAME,
                         help='The name of the embedding model to use (default: %(default)s)')
     parser.add_argument('--embed_model_provider', type=str, default=None,
                         help='The provider of the embedding model to use (default: %(default)s)')
@@ -253,11 +182,8 @@ def parse_arguments():
 
     args = parser.parse_args()
 
-    if not args.model:
-        args.model = args.model_url.split('/')[-1]
-
-    if str.lower(args.model) in MODEL_ALIASES.keys():
-        args.model = MODEL_ALIASES[args.model]
+    if str.lower(args.model) in config.Models.MODEL_ALIASES.keys():
+        args.model = config.Models.MODEL_ALIASES[args.model]
 
     return args
 
