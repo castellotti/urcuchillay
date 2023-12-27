4107231d49ca3fdaaadc6ae6e4ba7578005229d1
Steve Castellotti <sc@jigsaw.studio>
2023-12-27 01:01:20 -0800

    added handling for missing data directory and/or files

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ import os
 import sys
 
 import config
+import utils
 
 try:
     import llama_index
@@ -35,32 +36,46 @@ class Client:
         self.service_context = None
         self.index = None
 
-    def set_llm(self, args):
-        self.llm = llama_index.llms.OpenAI(
+    def get_llm(self, args):
+        return llama_index.llms.OpenAI(
             model="text-davinci-002",
             temperature=args.temperature,
             max_tokens=args.max_new_tokens,
-            callback_manager=self.callback_manager,
             api_base=config.APIConfig.get_openai_api_base(),
             api_key=config.APIConfig.OPENAI_API_KEY,
             api_version=config.APIConfig.OPENAI_API_VERSION,
+            callback_manager=self.callback_manager,
         )
 
-        self.service_context = llama_index.ServiceContext.from_defaults(
-            llm=self.llm,
+    def get_service_context(self, llm, args):
+        embed_model = config.Config.EMBED_MODEL_NAME
+        if hasattr(args, 'embed_model_name'):
+            if args.embed_model_name == 'default' or args.embed_model_name == 'local':
+                embed_model = args.embed_model_name
+            else:
+                if hasattr(args, 'embed_model_provider'):
+                    # use Huggingface embeddings
+                    embed_model = llama_index.embeddings.HuggingFaceEmbedding(
+                        model_name=args.embed_model_provider + '/' + args.embed_model_name)
+
+        return llama_index.ServiceContext.from_defaults(
+            llm=llm,
+            embed_model=embed_model,
+            callback_manager=self.callback_manager,
             context_window=args.context,
             num_output=args.max_new_tokens,
         )
 
-    def set_index(self, args):
-        if args.load and config.storage_verify(args.storage):
-            # load vector index from storage
-            storage_context = llama_index.StorageContext.from_defaults(persist_dir=args.storage)
-            self.index = llama_index.load_index_from_storage(storage_context, service_context=self.service_context)
+    def get_index(self, service_context, args):
+        if not os.path.exists(args.data) or not os.listdir(args.data):
+            # Create a temporary empty file for the index if a missing or empty data directory was supplied
+            temp_file = utils.create_temporary_empty_file()
+            documents = llama_index.SimpleDirectoryReader(input_files=[temp_file]).load_data()
+            index = llama_index.VectorStoreIndex.from_documents(documents, service_context=service_context)
+            os.remove(temp_file)
+            return index
         else:
-            # load documents
             documents = llama_index.SimpleDirectoryReader(args.data).load_data()
-            # create vector store index
-            self.index = llama_index.VectorStoreIndex.from_documents(
-                documents, service_context=self.service_context
+            return llama_index.VectorStoreIndex.from_documents(
+                documents, service_context=service_context
             )
