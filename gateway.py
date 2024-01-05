79400842394ad46da9f36285c0065f2ffa12976a
Steve Castellotti <sc@jigsaw.studio>
2024-01-05 03:50:26 -0800

    added /v0/gateway/load endpoint for requesting gateway to reload vector store

--- gateway.py
+++ gateway.py
@@ -51,14 +51,21 @@ gateway = Gateway(arguments)
 app = fastapi.FastAPI()
 
 
-@app.api_route("/v1/completions", methods=["POST"])
+@app.get('/v0/gateway/load')
+async def load_index():
+    """When the vector store is updated this service needs to reload it into memory"""
+    gateway.index = gateway.get_index(gateway.service_context, gateway.args)
+    return {'message': 'Index loaded successfully'}
+
+
+@app.api_route('/v1/completions', methods=['POST'])
 async def completions_endpoint(request_data: schemas.openai.CompletionsRequest, request: fastapi.Request):
     # Check if the content type is application/json
     if request.headers.get('Content-Type') != 'application/json':
-        raise fastapi.HTTPException(status_code=400, detail="Invalid Content-Type. Expected application/json.")
+        raise fastapi.HTTPException(status_code=400, detail='Invalid Content-Type. Expected application/json.')
 
     # Log the request data for debugging
-    logging.debug("Request Data:", request_data)
+    logging.debug('Request Data:', request_data)
 
     created = int(time.time())
 
@@ -67,15 +74,15 @@ async def completions_endpoint(request_data: schemas.openai.CompletionsRequest, 
         result = gateway.engine.query(request_data.prompt)
 
         response = {
-            "id": utils.generate_message_id(),
-            "object": "text_completion",
-            "created": created,
-            "model": request_data.model,
-            "choices": [
+            'id': utils.generate_message_id(),
+            'object': 'text_completion',
+            'created': created,
+            'model': request_data.model,
+            'choices': [
                 {
-                    "text": f"{result}",
-                    "index": 0,
-                    "finish_reason": "length",
+                    'text': f'{result}',
+                    'index': 0,
+                    'finish_reason': 'length',
                 },
             ],
         }
@@ -91,12 +98,12 @@ async def completions_endpoint(request_data: schemas.openai.CompletionsRequest, 
             tokens = list(streaming_response.response_gen)  # Convert generator to list
 
             for i, token in enumerate(tokens):
-                finish_reason = None if i < len(tokens) - 1 else "stop"
+                finish_reason = None if i < len(tokens) - 1 else 'stop'
 
                 choice = {
-                    "text": f"{token}",
-                    "index": 0,
-                    "finish_reason": finish_reason,
+                    'text': f'{token}',
+                    'index': 0,
+                    'finish_reason': finish_reason,
                 }
 
                 # Format the chunk as in the OpenAI API response
@@ -107,18 +114,18 @@ async def completions_endpoint(request_data: schemas.openai.CompletionsRequest, 
                     'model': request_data.model,
                     'choices': [choice],
                 }
-                chunk = f"data: {json.dumps(chunk_data)}\n\n"
+                chunk = f'data: {json.dumps(chunk_data)}\n\n'
                 yield chunk
 
-        return fastapi.responses.StreamingResponse(generate_responses(), media_type="text/event-stream")
+        return fastapi.responses.StreamingResponse(generate_responses(), media_type='text/event-stream')
 
 
-@app.api_route("/v1/chat/completions", methods=["POST"])
+@app.api_route('/v1/chat/completions', methods=['POST'])
 async def create_chat_completions(request_data: schemas.openai.ChatCompletionsRequest, request: fastapi.Request):
     if request.headers.get('Content-Type') != 'application/json':
-        raise fastapi.HTTPException(status_code=400, detail="Invalid Content-Type. Expected application/json.")
+        raise fastapi.HTTPException(status_code=400, detail='Invalid Content-Type. Expected application/json.')
 
-    logging.debug("Request Data:", request_data)
+    logging.debug('Request Data:', request_data)
 
     gateway.engine = gateway.index.as_chat_engine(chat_mode=gateway.chat_mode)
 
@@ -142,18 +149,18 @@ async def create_chat_completions(request_data: schemas.openai.ChatCompletionsRe
     if not request_data.stream:
         content = gateway.engine.chat(last_user_message.content)
         response = {
-            "id": message_id,
-            "object": "chat.completion",
-            "created": created,
-            "model": request_data.model,
-            "choices": [
+            'id': message_id,
+            'object': 'chat.completion',
+            'created': created,
+            'model': request_data.model,
+            'choices': [
                 {
-                    "index": 0,
-                    "message": {
-                        "role": "assistant",
-                        "content": content.response,
+                    'index': 0,
+                    'message': {
+                        'role': 'assistant',
+                        'content': content.response,
                     },
-                    "finish_reason": "stop"
+                    'finish_reason': 'stop'
                 }
             ]
         }
@@ -163,19 +170,19 @@ async def create_chat_completions(request_data: schemas.openai.ChatCompletionsRe
         # Use generator to handle streaming response
         def generate_responses():
             if last_user_message.role == schemas.openai.MessageRole.USER:
-                logging.info(f"User prompt: {last_user_message.content}")
+                logging.info(f'User prompt: {last_user_message.content}')
                 streaming_response = gateway.engine.stream_chat(last_user_message.content)
                 tokens = list(streaming_response.response_gen)  # Convert generator to list
 
                 for i, token in enumerate(tokens):
-                    finish_reason = None if i < len(tokens) - 1 else "stop"
+                    finish_reason = None if i < len(tokens) - 1 else 'stop'
 
                     choice = {
-                        "delta": {
-                            "content": f"{token}",
+                        'delta': {
+                            'content': f'{token}',
                         },
-                        "index": 0,
-                        "finish_reason": finish_reason,
+                        'index': 0,
+                        'finish_reason': finish_reason,
                     }
 
                     # Format the chunk as in the OpenAI API response
@@ -186,22 +193,22 @@ async def create_chat_completions(request_data: schemas.openai.ChatCompletionsRe
                         'object': 'chat.completion.chunk',
                         'choices': [choice],
                     }
-                    chunk = f"data: {json.dumps(chunk_data)}\n\n"
+                    chunk = f'data: {json.dumps(chunk_data)}\n\n'
                     yield chunk
 
         return schemas.openai.CustomStreamingResponse(generate_responses())
 
 
-@app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"])
+@app.api_route('/{path:path}', methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD', 'PATCH'])
 async def forward(request: fastapi.Request, path: str):
     # Forward the request to the OpenAI API server
-    url = f"http://{arguments.api_host}:{arguments.api_port}/{path}"
+    url = f'http://{arguments.api_host}:{arguments.api_port}/{path}'
 
     # Initialize body to None
     body = None
 
     # Check if the request is likely to have a body
-    if request.method in ["POST", "PUT", "PATCH"] and request.headers.get("Content-Type") == "application/json":
+    if request.method in ['POST', 'PUT', 'PATCH'] and request.headers.get('Content-Type') == 'application/json':
         try:
             body = await request.json()  # Attempt to parse JSON body
         except json.JSONDecodeError:
@@ -230,5 +237,5 @@ def main():
     uvicorn.run(app, host=arguments.host, port=arguments.port)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
